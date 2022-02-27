c9fe94ba932b6e11daea5368bf9e142479cdd534
Wellivelton <veltonmoura@gmail.com>
2022-02-27 11:45:57 -0400

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,22 @@
 class Pessoa:
+    def __init__(self, *filhos, nome = None, idade = 29, sexo = 'Masculino'):
+        self.sexo = sexo
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(id(p))
-    print(Pessoa.cumprimentar(p))
-    print(p.cumprimentar())
+    renzo = Pessoa(nome='Renzo')
+    velton = Pessoa(renzo, nome='Velton')
+    print(id(velton))
+    print(Pessoa.cumprimentar(velton))
+    print(velton.cumprimentar())
+    print(velton.nome)
+    print(velton.idade)
+    print(velton.sexo)
+    #print(velton.filhos)
+    for filho in velton.filhos:
+        print(filho.nome)
